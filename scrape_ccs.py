d7e3c63952e70b1b9675b02e52e5ed3da756da62
plowsof <77655812+plowsof@users.noreply.github.com>
2022-09-19 13:13:56 +0100

    oldest first + luigi mode

--- scrape_ccs.py
+++ scrape_ccs.py
@@ -12,6 +12,8 @@ ideas = soup.children
 
 ideas_data = []
 
+# first come, first serve
+
 for idea in ideas:
 	try:
 		for item in idea.find_all('a'):
@@ -21,6 +23,17 @@ for idea in ideas:
 			ideas_data.append(data)
 	except:
 		pass
+
+# order list first = last
+ideas_len = len(ideas_data)
+ordered = [None]*ideas_len
+for i in range(len(ideas_data)):
+	ideas_len-=1
+	print(f"{ideas_len} ideas_len")
+	print(i)
+	ordered[ideas_len]=ideas_data[i]
+ideas_data=ordered
+
 
 template = """<pre>Plowsof has just saved you atleast 15 minutes of your life, do the needful sir:
 88UbURD1esv6wJNqLEvJFn82JbX6awYW2BW4Sj2E3rahQbxP2C4FgGS6tdLp2wdPHJZ4PjgjuZbfi139oZ3LK9gcMXugv7C
@@ -42,7 +55,7 @@ Proposed Meeting Items:
 2. Greetings
 3. Community highlights    
 News: [Monero Observer](https://www.monero.observer/) - [Monero Moon](https://www.themoneromoon.com/) - [Revuo Monero](https://revuo-xmr.com/)
-4. [CCS updates](https://ccs.getmonero.org/)    """
+4. [CCS updates](https://ccs.getmonero.org/)    </br>"""
 
 i = 0
 for idea in ideas_data:
@@ -69,7 +82,26 @@ template += """5. Workgroup reports    </br>
 
 Meeting logs will be posted here afterwards.    </pre>"""
 
-with open("/var/www/html/meeting_template.html", "w+") as f:
+template += """</br></br><pre>
+
+Luigi1111_ccs_merges_mode:
+
+Greetings, my internet is not reliable so we need to get through the merge list asap
+merge list is here: https://repo.getmonero.org/monero-project/ccs-proposals/-/merge_requests
+
+"""
+
+i = 0
+for idea in ideas_data:
+	x=(i+1)
+	try:
+		template += f"  {d[x]}. [{idea['title']}]({idea['href']})    \n"
+	except Exception as e:
+		raise e
+		pass
+	i+=1
+template += "</pre>"
+with open("meeting_template.html", "w+") as f:
 	f.write(template)
 
 print("done")
